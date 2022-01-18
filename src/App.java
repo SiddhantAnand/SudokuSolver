69ac0ffcbf7a886e168132825b4ef6824d32c59e
siddhanta_kloudgin <siddhanta@kloudgin.com>
2022-01-19 01:20:51 +0530

    Added print statements and few validations

--- src/App.java
+++ src/App.java
@@ -2,19 +2,25 @@ public class App {
     public static void main(String[] args) throws Exception {
         Solve solve = new Solve();
         int[][] board = {
-            {7,0,2,0,5,0,6,0,0},
-            {0,0,0,0,0,3,0,0,0},
-            {1,0,0,0,0,9,5,0,0},
-            {8,0,0,0,0,0,0,9,0},
-            {0,4,3,0,0,0,7,5,0},
-            {0,9,0,0,0,0,0,0,8},
-            {0,0,9,7,0,0,0,0,5},
-            {0,0,0,2,0,0,0,0,0},
-            {0,0,7,0,4,0,2,0,3}
+            {6,0,0,0,0,8,0,5,0},
+            {2,0,5,4,0,0,0,0,9},
+            {3,0,4,0,0,0,0,0,0},
+            {0,0,0,9,4,0,0,0,0},
+            {0,0,0,0,0,0,8,1,0},
+            {0,0,0,0,0,0,0,0,7},
+            {0,9,0,0,0,0,0,7,0},
+            {4,0,0,0,6,1,0,8,0},
+            {0,0,0,0,0,0,5,0,3}
         };
+        
+        System.out.println("Unsolve board look like..");
         solve.printSudoku(board);
-        solve.solve(board);
-        System.out.println("Solved!");
-        solve.printSudoku(board);
+        if(solve.solve(board)){
+            System.out.println("\n\n We are ready , the sudoku is solved!");
+            solve.printSudoku(board);
+        }else{
+            System.out.println("Cannot be solved");
+        }
+        
     }
 }
